644c87e6815d24ad0642e63c3c530db6127ec64e
Hakan Ömer ÖZTEK <hakanoztek@gmail.com>
2017-10-30 14:26:15 +0300

    printf kullanımı

---
+++ hafta01/kod2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    printf("a=%d\n", 3);
+    printf("b=%f\n", 5.987);
+    printf("Merhaba Bayan %c\n", 'A');
+    printf("Selam %s\n", "Ahmet");
+    printf("============\n");
+    printf("a=%d\nb=%f\nMerhaba Bayan %c\nSelam %s\n", 3,5.987,'A',"Ahmet");
+    return 0;
+}
