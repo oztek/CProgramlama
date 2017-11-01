3531b25441c722ab60db047a0bb1f34637827f04
Hakan Ömer ÖZTEK <hakanoztek@gmail.com>
2017-11-01 14:55:13 +0300

    kod3 eklendi

---
+++ hafta01/kod3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int sayi1;
+    int sayi2=3;
+    int sayi3=5, sayi4;
+    sayi1=8;
+    sayi4=sayi3+5;
+    sayi1=sayi3+sayi4;
+    printf("%d %d %d %d\n", sayi1, sayi2, sayi3, sayi4);
+    return 0;
+}
